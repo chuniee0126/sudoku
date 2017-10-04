73da05cd22208269c2ccf1e53dd35a68b92c1865
master <chunie0126@gmail.com>
2017-10-05 04:28:03 +0900

    modified : parameter issue

--- Sudoku/create_formula.c
+++ Sudoku/create_formula.c
@@ -28,26 +28,26 @@ bool write_CP4(FILE * formula){
 	return true;
 }
 
-bool write_CP5(int * a_input, FILE * formula) {
-	int * sudoku = a_input;
-	int pos[10]; //asterisk(*) position(store the index)
-	int i, j, pos_count = 0;
-	for (i = 0; i < 81; i++)
-	{
-		if (sudoku[i] == '*') //If the received value is asterisk(*)
-		{
-			pos[pos_count] = i;
-			pos_count++;
-		}
-	}
-	for (i = 0; i < pos_count - 1; i++)
-	{
-		for (j = 1; j <= 9; j++)
-		{
-			fprintf(formula, "%d -%d 0\n", pos[i] * 9 + j, pos[i + 1] * 9 + j);
-			fprintf(formula, "-%d %d 0\n", pos[i] * 9 + j, pos[i + 1] * 9 + j);
-		}
-	}
+bool write_CP5(FILE * input, FILE * formula) {
+	// int * sudoku = a_input;
+	// int pos[10]; //asterisk(*) position(store the index)
+	// int i, j, pos_count = 0;
+	// for (i = 0; i < 81; i++)
+	// {
+	// 	if (sudoku[i] == '*') //If the received value is asterisk(*)
+	// 	{
+	// 		pos[pos_count] = i;
+	// 		pos_count++;
+	// 	}
+	// }
+	// for (i = 0; i < pos_count - 1; i++)
+	// {
+	// 	for (j = 1; j <= 9; j++)
+	// 	{
+	// 		fprintf(formula, "%d -%d 0\n", pos[i] * 9 + j, pos[i + 1] * 9 + j);
+	// 		fprintf(formula, "-%d %d 0\n", pos[i] * 9 + j, pos[i + 1] * 9 + j);
+	// 	}
+	// }
 
 	return true;
 }
