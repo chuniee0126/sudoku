6766a9f3bd5dd24937f6bdaeee23a1a722516efc
Jeonghwan Kim <jeonghwankim123@gmail.com>
2017-10-05 03:44:09 +0900

    write_CP5 - Jeonghwan

--- Sudoku/create_formula.c
+++ Sudoku/create_formula.c
@@ -4,11 +4,11 @@
 
 #include "create_formula.h"
 
-bool write_firstline(FILE * input, FILE * formula){
+bool write_firstline(int * a_input, FILE * formula){
 	return true;
 }
 
-bool write_inputvalue(FILE * input, FILE * formula){
+bool write_inputvalue(int * a_input, FILE * formula){
 	return true;
 }
 
@@ -28,6 +28,26 @@ bool write_CP4(FILE * formula){
 	return true;
 }
 
-bool write_CP5(FILE * input, FILE * formula){
+bool write_CP5(int * a_input, FILE * formula) {
+	int * sudoku = a_input;
+	int pos[10]; //asterisk(*) position(store the index)
+	int i, j, pos_count = 0;
+	for (i = 0; i < 81; i++)
+	{
+		if (sudoku[i] == '*') //If the received value is asterisk(*)
+		{
+			pos[pos_count] = i;
+			pos_count++;
+		}
+	}
+	for (i = 0; i < pos_count - 1; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			fprintf(formula, "%d -%d 0\n", pos[i] * 9 + j, pos[i + 1] * 9 + j);
+			fprintf(formula, "-%d %d 0\n", pos[i] * 9 + j, pos[i + 1] * 9 + j);
+		}
+	}
+
 	return true;
 }
--- Sudoku/sudoku.h
+++ Sudoku/sudoku.h
@@ -1,26 +1,26 @@
-/**
- * @file sudoku.h
- * @brief
- * @author Chunmyong Park, "", ""
- * @date October 4, 2017
- */
-#ifndef SUDOKU_H
-#define SUDOKU_H
-
-/**
- * @brief Create formula.txt file
- */
-bool create_formula(FILE * input, FILE * formula);
-
-/**
- * @brief Run dimacs program with formula.txt
- *        and create output array
- */
-int* run_dimacs(FILE * formula, int * a_output);
-
-/**
- * @brief Create Output.txt file
- */
-bool create_output(FILE * output, int * result);
-
-#endif
+///**
+// * @file sudoku.h
+// * @brief
+// * @author Chunmyong Park, "", ""
+// * @date October 4, 2017
+// */
+//#ifndef SUDOKU_H
+//#define SUDOKU_H
+//
+///**
+// * @brief Create formula.txt file
+// */
+//bool create_formula(FILE * input, FILE * formula);
+//
+///**
+// * @brief Run dimacs program with formula.txt
+// *        and create output array
+// */
+//bool run_dimacs(FILE * formula, int * a_output);
+//
+///**
+// * @brief Create Output.txt file
+// */
+//bool create_output(FILE * output, int * result);
+//
+//#endif
