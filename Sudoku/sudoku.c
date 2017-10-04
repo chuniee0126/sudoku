60b55588a938cf86c3000645d948ae8f3d53825d
Jeonghwan Kim <jeonghwankim123@gmail.com>
2017-10-05 04:04:50 +0900

    write_CP5 - Jeonghwan

--- Sudoku/sudoku.c
+++ Sudoku/sudoku.c
@@ -1,18 +1,18 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <stdbool.h>
-//
-//#include "create_formula.h"
-//#include "sudoku.h"
-//
-//bool create_formula(FILE * input, FILE * formula){
-//	true;
-//}
-//
-//bool run_dimacs(FILE * formula, int * a_output){
-//	true;
-//}
-//
-//bool create_output(FILE * output, int * result){
-//	true;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "create_formula.h"
+#include "sudoku.h"
+
+bool create_formula(FILE * input, FILE * formula){
+	true;
+}
+
+bool run_dimacs(FILE * formula, int * a_output){
+	true;
+}
+
+bool create_output(FILE * output, int * result){
+	true;
+}
